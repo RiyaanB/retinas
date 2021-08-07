0daba19786803e7b1ac1c83c224c734842e22a05
Riyaan Bakhda <riyaanbakhda2@gmail.com>
2021-08-07 14:32:18 +0530

    Added RetinaBody and RetinaCamera

--- objects.py
+++ objects.py
@@ -1,6 +1,7 @@
 import numpy as np
 from representation import *
 from pose import *
+import apriltag
 
 MIN_POINTS = 4
 MIN_REPRESENTATION = 1
@@ -14,33 +15,24 @@ def __is_iterable__(x):
     return not (('__iter__' not in dir(x)) or (x.__iter__ is None))
 
 
-class Body:
+class RetinaBody:
 
-    def __init__(self, labels, points, representation=None, pose=None):
-        assert __is_iterable__(labels)
-        assert isinstance(points, np.matrix) or isinstance(points, np.ndarray)
-        assert points.shape[0] == 3
-        assert points.shape[1] >= MIN_POINTS
-        assert len(labels) == points.shape[1]
-        for label in labels:
-            assert __is_hashable__(label)
-        assert representation is None or isinstance(representation, Representation)
+    def __init__(self, point_dict, pose=((0, 0, 0), (0, 0, 0)), representation=None):
+        self.point_dict = point_dict
+        if representation is None:
+            self.representation = Representation(point_dict)
+        else:
+            self.representation = representation
 
-        self.labels = labels
-        self.points = points
-        self.representation = representation
-
-        if pose is None:
-            pose = Pose(0, 0, 0, 0, 0, 0)
-        self.pose = pose
+        self.pose = Pose(pose)
 
 
-class Camera:
+class RetinaCamera:
 
-    def __init__(self, streamer, representation=None, pose=None):
+    def __init__(self, streamer, get_view, pose=((0, 0, 0), (0, 0, 0)), representation=None):
         self.streamer = streamer
+        if representation is None:
+            representation = AxisCamera(streamer.name)
         self.representation = representation
-        self.pose = pose
-
-    def get_view(self):
-        pass
+        self.pose = Pose(pose)
+        self.get_view = get_view
