16f14f22d8f6b2819974bcfaf0f9a2708c8e256e
Riyaan <riyaanbakhda2@gmail.com>
2022-05-12 20:25:34 -0400

    Added global optimization and sticky option

--- retinas.py
+++ retinas.py
@@ -1,6 +1,6 @@
 import apriltag
 import cv2
-import utils.camera_streamer as cs
+import Retinas.utils.camera_streamer as cs
 import numpy as np
 from threading import Thread
 import time
@@ -10,12 +10,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-from objects import RetinaCamera, RetinaBody
-from pose import Pose, get_cam_pose
-from test_bodies.cube_body import cube0_body, cube1_body
-from test_bodies.world_body_4_corners import world_body
-from utils.convex_hull import get_convex_hull_area
-from world import World
+from Retinas.objects import RetinaCamera, RetinaBody
+from Retinas.pose import Pose, get_cam_pose
+from Retinas.test_bodies.cube_body import cube0_body, cube1_body
+from Retinas.test_bodies.world_body_4_corners import world_body
+from Retinas.utils.convex_hull import get_convex_hull_area
+from Retinas.world import World
 
 DEFAULT_APRILTAG_DETECTOR = apriltag.Detector()
 STRENGTH_CONSTANT = 1   # k
@@ -25,7 +25,18 @@ class ApriltagObserver:
     def __init__(self, camera_streamer, threshold=True):
         self.camera_streamer = camera_streamer
         self.threshold = threshold
-        self.detector = apriltag.Detector()
+        self.detector = apriltag.Detector(apriltag.DetectorOptions(
+            families='tag36h11',
+            border=1,
+            nthreads=8,
+            quad_decimate=1.0,
+            quad_blur=0.0,
+            refine_edges=True,
+            refine_decode=False,
+            refine_pose=False,
+            debug=False,
+            quad_contours=True
+            ))
 
         self.grayscale_frame = None
         self.frame = None
@@ -38,27 +49,33 @@ class ApriltagObserver:
 
         grayscale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         if self.threshold:
+            # grayscale_frame = cv2.GaussianBlur(grayscale_frame, (3,3),0)
             _ , grayscale_frame = cv2.threshold(grayscale_frame, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
         results = self.detector.detect(grayscale_frame)
 
         # print("After detect")
+        tag_list = []
         label_list = []
         point_list = []
         for r in results:
+            tag_list.append(r.tag_id)
             for corner in range(4):
                 label_list.append((r.tag_id, corner))
-                frame = cv2.circle(frame, np.array(r.corners[corner], dtype=np.int32), radius=0, color=(0,0, corner*63), thickness=32)
+                frame = cv2.circle(frame, np.array(r.corners[corner], dtype=np.int32), radius=0, color=(0,0, corner*63), thickness=16)
                 point_list.append(r.corners[corner])
         # cv2.imshow(self.camera_streamer.name, grayscale_frame)
         # print("Showed")
-        # print(len(label_list))
+        # print(tag_list)
+        if len(tag_list) < 3:
+            return self.get_observation()
         self.frame = frame
+        self.grayscale_frame = grayscale_frame
         return label_list, np.array(point_list)
 
 
 class Retinas(Thread):
 
-    def __init__(self, observers, bodies, cameras):
+    def __init__(self, observers, bodies, cameras, sticky=True):
         super().__init__()
 
         self.observers = observers
@@ -66,6 +83,7 @@ class Retinas(Thread):
         self.cameras = cameras
         self.J = len(observers)
         self.I = len(bodies)
+        self.sticky = sticky
 
         self.tag_map = {}
         for body in bodies:
@@ -90,8 +108,12 @@ class Retinas(Thread):
             T = {}
             E = {}
             G = {}
-            world_camera_poses = self.world_camera_poses
-            world_body_poses = self.world_body_poses
+            if self.sticky:
+                world_camera_poses = self.world_camera_poses
+                world_body_poses = self.world_body_poses
+            else:
+                world_camera_poses = {}#self.world_camera_poses
+                world_body_poses = {}#self.world_body_poses
             counter = 0
             for j in range(J):
                 k_labels, k_points = self.observers[j].get_observation()
@@ -116,7 +138,7 @@ class Retinas(Thread):
                 T[j,i] = self.do_pnp(labels, points, observer, body)
                 E[j,i] = self.get_total_reprojection_error(labels, points, observer, body, T[j,i])[0]
                 # print(E[j,i])
-                temp = (len(N[j, i][0])**0.5) * A[j,i] * E[j,i]
+                temp = (len(N[j, i][0])**0.5) * E[j,i]
                 G[j,i] = - np.log(1 + np.exp(-STRENGTH_CONSTANT) * temp)
 
             ####################
@@ -186,7 +208,7 @@ class Retinas(Thread):
             visible_body[1].append(body.point_dict[label])
 
         projected, jacobian = cv2.projectPoints(np.array(visible_body[1]), pose.rvec, pose.tvec, observer.camera_streamer.K, observer.camera_streamer.D)
-        error = np.sum(np.power(projected-points, 2))
+        error = np.sum(np.power(np.squeeze(projected)-points, 2))
         jacobian_final = None
         return error, jacobian_final
 
--- world.py
+++ world.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
-from pose import *
-from representation import *
-from objects import *
-import utils.camera_streamer as cs
+from Retinas.pose import *
+from Retinas.representation import *
+from Retinas.objects import *
+import Retinas.utils.camera_streamer as cs
 import threading
 
 DEFAULT_CAMERA_POSITION = (0, -1.8, 1)
@@ -16,9 +16,10 @@ PI = np.pi
 DEBUG = False
 
 
-class World:
+class World(threading.Thread):
 
     def __init__(self, name, bodies, cameras, camera_pose=DEFAULT_WORLD_CAM_POSE, camera_dims=DEFAULT_CAMERA_DIMS, K=DEFAULT_K, D=DEFAULT_D):
+        super().__init__()
         self.bodies = bodies
         self.cameras = cameras
         self.name = name
@@ -26,7 +27,9 @@ class World:
         self.K = K
         self.D = D
         self.camera_pose = camera_pose
+        self.__is__running__ = True
         cv2.namedWindow(name)
+        self.start()
 
     def draw(self):
         frame = np.zeros(self.camera_dims)
@@ -48,13 +51,24 @@ class World:
         return frame
 
     def display(self):
-        cv2.imshow(self.name, self.draw())
-        k = cv2.waitKey(1)
-        if k % 256 == 27:
-            # ESC pressed
-            print("Escape hit, closing...")
-            for a in range(5):
-                cv2.destroyWindow(self.name)
+        disp_img = self.draw()
+        cv2.imshow(self.name, disp_img)
+        cv2.imwrite(self.name + ".png", disp_img)
+        k = cv2.waitKey(1000)
+        cv2.destroyWindow(self.name)
+        # if k % 256 == 27:
+        #     # ESC pressed
+        #     print("Escape hit, closing...")
+        #     for a in range(5):
+        #         cv2.destroyWindow(self.name)
+
+    def run(self):
+        print("RUNNNING")
+        while self.__is__running__:
+            print("Drawing...")
+            self.display()
+            print("Drew!")
+        print("QUITTING")
 
 if __name__ == '__main__':
     bodies = []
